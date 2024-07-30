487924a2f0db2c890ed43bcfb2d01a9c4cf41a12
ovaserashid <159573241+Ovaserashid@users.noreply.github.com>
2024-07-30 16:42:46 +0530

    Categorize buttons in html and change javascript
    Instead of selecting all buttons and then applying
    conditions, categorized the type of buttons in html
    file as numbers, operations, equal key, dot key.
    Now when a particular type of button is pressed
    conditions are checked, if conditions met only then
    certain actions occur. For example if a number is
    pressed and no operation has occurred yet, the number
    will be pushed in array1, if a number is pressed
    and operation has been pressed then the number will
    be pushed to array2, and so on.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,20 @@
-let number1=[];
-let number2=[];
+const buttonNumber = document.querySelectorAll('.numbers');
+const buttonEqual = document.querySelector('.equal');
+const buttonDot = document.querySelectorAll('.dot');
+const buttonOperation = document.querySelectorAll('.operation');
+const display = document.querySelector('.display');
+
+let numArray1=[];
+let numArray2=[];
 let firstNumber = false;
 let secondNumber = false;
 let operation = false;
 let operator;
-let result = false;
-const button = document.querySelectorAll('button');
-const display = document.querySelector('.display');
+let result;
+let number1;
+let number2;
 
 function finalResult(num1, num2){
-    num1=+num1.join('');
-    num2=+num2.join('');
     if(operator === '+'){
         return num1+num2;
     }else if(operator === '-'){
@@ -23,33 +27,70 @@ function finalResult(num1, num2){
 }
 
 function displayResult(value){
-    value = value.join('');
-    if(result === false){
-        display.textContent = value;
-    }else if(result === true){
-        display.textContent = finalResult(number1, number2);
-    }
-    
+    display.textContent = value;    
 }
-button.forEach((btn)=>{
+
+// button.forEach((btn)=>{
+//     btn.addEventListener('click', (e)=>{
+//         if(btn.value === '+' || btn.value === '-' || btn.value === '/' || btn.value === '*' ){
+//             firstNumber = true
+//             operation = true;
+//             operator = btn.value;
+//         }else if(btn.value === '=' && operation === true){
+//             console.log('yes');
+//             result = true;
+//             display.textContent = finalResult(number1, number2);
+//         }
+//         if(firstNumber === false && operation === false){
+//             number1.push(btn.value);
+//             displayResult(number1);
+//         // else if(firstNumber === true && secondNumber === false && operation === true){
+//         //     operation = false;
+//         }else if(firstNumber === true && secondNumber === false && operation === false && result === false){
+//             number2.push(btn.value);
+//             displayResult(number2);
+//         }
+//     });
+// });
+buttonNumber.forEach((btn)=>{
     btn.addEventListener('click', (e)=>{
-        if(btn.value === '+' || btn.value === '-' || btn.value === '/' || btn.value === '*'){
+        if(operation === false){
+            numArray1.push(btn.value);
+            number1= +numArray1.join('');
+            displayResult(number1);
             firstNumber = true;
-            operation = true;
-            operator = btn.value;
-        }else if(btn.value === '='){
-            console.log('yes');
-            result = true;
-            display.textContent = finalResult(number1, number2);
-        }
-        if(firstNumber === false && secondNumber === false){
-        number1.push(btn.value);
-        displayResult(number1);
-        }else if(firstNumber === true && secondNumber === false && operation === true){
-            operation = false;
-        }else if(firstNumber === true && secondNumber === false && operation === false && result === false){
-            number2.push(btn.value);
+        }else if(firstNumber === true && operation === true){
+            numArray2.push(btn.value);
+            number2= +numArray2.join('');
             displayResult(number2);
+            secondNumber = true;
         }
     });
 });
+buttonOperation.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        if(firstNumber === true && secondNumber === false){
+            operator = btn.value;
+            operation =true;
+        }else if(firstNumber === true && secondNumber === true){
+            result = finalResult(number1, number2);
+            displayResult(result);
+            number1 = result;
+            secondNumber = false;
+            numArray2 = [];
+            operator = btn.value;
+            operation = true;
+        }
+    });
+});
+buttonEqual.addEventListener('click', (btn)=>{
+    if(firstNumber === true && secondNumber === true){
+        result = finalResult(number1, number2);
+        displayResult(result);
+        numArray1 = [];
+        numArray2 = [];
+        firstNumber = false;
+        secondNumber = false;
+        operation = false;
+    }
+})
