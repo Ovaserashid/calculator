0cc920154532a95a53219525eb48b77555169f77
ovaserashid <159573241+Ovaserashid@users.noreply.github.com>
2024-07-30 19:38:36 +0530

    Round result to specific digits and more dot logic
    Rounded the result value to 9 decimal places. Also
    added a logic when dot is pressed directly without
    0 results into 0. in the display.

--- javascript.js
+++ javascript.js
@@ -14,6 +14,7 @@ let result;
 let number1;
 let number2;
 let dot = false;
+let equalPressed = false;
 
 function finalResult(num1, num2){
     if(operator === '+'){
@@ -31,28 +32,6 @@ function displayResult(value){
     display.textContent = value;    
 }
 
-// button.forEach((btn)=>{
-//     btn.addEventListener('click', (e)=>{
-//         if(btn.value === '+' || btn.value === '-' || btn.value === '/' || btn.value === '*' ){
-//             firstNumber = true
-//             operation = true;
-//             operator = btn.value;
-//         }else if(btn.value === '=' && operation === true){
-//             console.log('yes');
-//             result = true;
-//             display.textContent = finalResult(number1, number2);
-//         }
-//         if(firstNumber === false && operation === false){
-//             number1.push(btn.value);
-//             displayResult(number1);
-//         // else if(firstNumber === true && secondNumber === false && operation === true){
-//         //     operation = false;
-//         }else if(firstNumber === true && secondNumber === false && operation === false && result === false){
-//             number2.push(btn.value);
-//             displayResult(number2);
-//         }
-//     });
-// });
 buttonNumber.forEach((btn)=>{
     btn.addEventListener('click', (e)=>{
         if(operation === false){
@@ -77,6 +56,7 @@ buttonOperation.forEach((btn)=>{
             dot = false;
         }else if(firstNumber === true && secondNumber === true){
             result = finalResult(number1, number2);
+            result = parseFloat(result.toFixed(9));
             displayResult(result);
             number1 = result;
             secondNumber = false;
@@ -84,12 +64,19 @@ buttonOperation.forEach((btn)=>{
             operator = btn.value;
             operation = true;
             dot = false;
+        }else if(firstNumber === false && equalPressed === true){
+            number1 = result;
+            firstNumber = true;
+            operation = true;
+            operator = btn.value;
+            equalPressed = false;
         }
     });
 });
 buttonEqual.addEventListener('click', (btn)=>{
     if(firstNumber === true && secondNumber === true){
         result = finalResult(number1, number2);
+        result = parseFloat(result.toFixed(9));
         displayResult(result);
         numArray1 = [];
         numArray2 = [];
@@ -97,18 +84,31 @@ buttonEqual.addEventListener('click', (btn)=>{
         secondNumber = false;
         operation = false;
         dot = false;
+        equalPressed = true;
     }
 });
 buttonDot.addEventListener('click', (btn)=>{
     if(!dot){
-        if(operation === false){
+        if(operation === false && firstNumber === true){
             numArray1.push('.');
-            number1 = +numArray1.join('');
+            number1 = numArray1.join('');
             dot = true;
-        }else if(firstNumber === true && operation === true){
+            displayResult(number1);
+        }else if(operation === true && secondNumber === true){
             numArray2.push('.');
-            number2 = +numArray2.join('');
+            number2 = numArray2.join('');
             dot = true;
+            displayResult(number2);
+        }else if(firstNumber === false){
+            numArray1.push('0','.');
+            number1 = numArray1.join('');
+            dot = true;
+            displayResult(number1);
+        }else if(operation === true && secondNumber === false){
+            numArray2.push('0','.');
+            number2 = numArray2.join('');
+            dot = true;
+            displayResult(number2);
         }
     }
 });
